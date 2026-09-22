4bcdeb63666fb3aa90bc5ba224139124d5fb6bdf
Resbi-Anik <resbianik13@gmail.com>
2026-09-22 02:28:20 -0400

    Task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -13,12 +13,22 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  return users.some((user) => user.username === username && user.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+    return res.status(400).json({message: "Username and password are required"});
+  }
+  if (!authenticatedUser(username, password)) {
+    return res.status(401).json({message: "Invalid username or password"});
+  }
+  const accessToken = jwt.sign({data: password}, "access", {expiresIn: 60 * 60});
+  req.session.authorization = {accessToken, username};
+  return res.status(200).json({message: "User successfully logged in"});
 });
 
 // Add a book review
