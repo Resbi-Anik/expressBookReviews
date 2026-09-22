17466543b7bfd319d4d8354b75b6bcd66871ce5d
Resbi-Anik <resbianik13@gmail.com>
2026-09-22 02:19:53 -0400

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -44,9 +44,19 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const title = req.params.title;
+    const matchingBooks = [];
+    Object.keys(books).forEach((isbn) => {
+      if (books[isbn].title === title) {
+        matchingBooks.push({isbn: isbn, ...books[isbn]});
+      }
+    });
+    if (matchingBooks.length > 0) {
+      return res.status(200).json({booksbytitle: matchingBooks});
+    } else {
+      return res.status(404).json({message: "No books found for this title"});
+    }
+  });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
